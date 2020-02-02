90a54625315b6e6e24e04ffb6bb130c20361a7c8
richard dinh <richardndinh@gmail.com>
2020-02-02 03:24:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,14 @@
-
 def prime?(x)
-  if x == 1 
-     return true 
-  elsif x < 1 
-     return false
-   else 
-     for y in 2..(x - 1)
-      if (x % y) == 0 
-       return false
-     end
-   end
+  if x == 1
+    return true 
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
 end
 
   true
